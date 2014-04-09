8eafc71498e7e6b634952850613fb6c2b85ee1ff
garbrand <garbrand@gmail.com>
2014-04-09 17:05:55 +0200

    Added mongo, vbox and vmip

---
+++ mongo.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# To have launchd start mongodb at login:
+#     ln -sfv /usr/local/opt/mongodb/*.plist ~/Library/LaunchAgents
+# Then to load mongodb now:
+#     launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist
+# Or, if you don't want/need launchctl, you can just run:
+#     mongod
