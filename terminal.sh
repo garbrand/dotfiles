87bb64325e1e007a60fc1aeba5aa599e686945fd
garbrand <garbrand@gmail.com>
2015-09-25 15:39:04 +0200

    Merged folder.sh into terminal.sh, changed default behaviour of folder()

--- terminal.sh
+++ terminal.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
 function tabname {
+    # Set the tabname, if no argument given, to the current folder
+    if [ -z "$1" ]; then
+        FOLDER=$(folder)
+    else
+        FOLDER=$1
+    fi
+
 	# directory = ${PWD##*/}
-  printf "\e]1;$1\a"
+  printf "\e]1;$FOLDER\a"
 }
- 
+
 function winname {
   printf "\e]2;$1\a"
+}
+
+function folder {
+	# Echo the name of the current directory to stdout.
+	# Given the directory is /Users/Garbrand/somewhere,
+	# this command will echo `somewhere`
+	printf '%s\n' "${PWD##*/}"
 }
