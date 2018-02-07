ad134e8bc0ada0c9a2315a2ba1a34c4ceca16ffc
Garbrand van der Molen <garbrand@gmail.com>
2018-02-07 22:22:20 +0100

    Added a COMMAND     PID     USER   FD   TYPE             DEVICE SIZE/OFF NODE NAME launchd       1     root   18u  IPv4 0xa173bb43b161b07b      0t0  TCP *:5900 (LISTEN) launchd       1     root   19u  IPv6 0xa173bb43b161445b      0t0  TCP *:5900 (LISTEN) launchd       1     root   21u  IPv4 0xa173bb43b161b07b      0t0  TCP *:5900 (LISTEN) launchd       1     root   22u  IPv6 0xa173bb43b1614a1b      0t0  TCP *:445 (LISTEN) launchd       1     root   26u  IPv6 0xa173bb43b161445b      0t0  TCP *:5900 (LISTEN) launchd       1     root   28u  IPv6 0xa173bb43b1614a1b      0t0  TCP *:445 (LISTEN) launchd       1     root   29u  IPv4 0xa173bb43b161b9cb      0t0  TCP *:445 (LISTEN) launchd       1     root   31u  IPv6 0xa173bb43b1613e9b      0t0  TCP *:548 (LISTEN) launchd       1     root   32u  IPv4 0xa173bb43b161b9cb      0t0  TCP *:445 (LISTEN) launchd       1     root   45u  IPv6 0xa173bb43b1613e9b      0t0  TCP *:548 (LISTEN) launchd       1     root   46u  IPv4 0xa173bb43b161c31b      0t0  TCP *:548 (LISTEN) launchd       1     root   47u  IPv4 0xa173bb43b161c31b      0t0  TCP *:548 (LISTEN) node         75     root   11u  IPv4 0xa173bb43b161d5bb      0t0  TCP localhost:9998 (LISTEN) kdc         112     root    5u  IPv6 0xa173bb43b1614fdb      0t0  TCP *:88 (LISTEN) kdc         112     root    7u  IPv4 0xa173bb43b161a72b      0t0  TCP *:88 (LISTEN) awacsd      226     root    8u  IPv6 0xa173bb43b7dca8db      0t0  TCP [fdb6:679c:d12d:d038:d161:ba1a:acd1:6647]:4488 (LISTEN) mongod      572 garbrand    6u  IPv4 0xa173bb43d60115bb      0t0  TCP *:27017 (LISTEN) cupsd       956     root    5u  IPv6 0xa173bb43b769ee9b      0t0  TCP localhost:631 (LISTEN) cupsd       956     root    6u  IPv4 0xa173bb43ca43a85b      0t0  TCP localhost:631 (LISTEN) Synology   2311 garbrand   23u  IPv4 0xa173bb43b161948b      0t0  TCP localhost:1024 (LISTEN) client-ma  2351 garbrand    6u  IPv4 0xa173bb43c446072b      0t0  TCP localhost:1025 (LISTEN) client-ma  2351 garbrand   23u  IPv4 0xa173bb43b161948b      0t0  TCP localhost:1024 (LISTEN) Adobe\x20  4811     coby    8u  IPv4 0xa173bb43b7367ddb      0t0  TCP localhost:15292 (LISTEN) Dropbox    4910     coby  127u  IPv6 0xa173bb440c08da1b      0t0  TCP *:17501 (LISTEN) Dropbox    4910     coby  128u  IPv4 0xa173bb43d2a95b3b      0t0  TCP *:17501 (LISTEN) Dropbox    4910     coby  160u  IPv4 0xa173bb43ca43831b      0t0  TCP localhost:17601 (LISTEN) Dropbox   45600 garbrand  124u  IPv4 0xa173bb43b0d791ab      0t0  TCP *:17500 (LISTEN) Dropbox   45600 garbrand  126u  IPv6 0xa173bb43d7dbb59b      0t0  TCP *:17500 (LISTEN) Dropbox   45600 garbrand  157u  IPv4 0xa173bb43c3e8148b      0t0  TCP localhost:17600 (LISTEN) Dropbox   45600 garbrand  163u  IPv4 0xa173bb43d5215ddb      0t0  TCP localhost:17603 (LISTEN) Spotify   91572 garbrand   34u  IPv4 0xa173bb43d34d6b3b      0t0  TCP localhost:4381 (LISTEN) Spotify   91572 garbrand   70u  IPv4 0xa173bb43d600d48b      0t0  TCP *:57621 (LISTEN) Spotify   91572 garbrand   75u  IPv4 0xa173bb43d369772b      0t0  TCP *:59246 (LISTEN) SpotifyWe 91580 garbrand    5u  IPv4 0xa173bb43d6021ddb      0t0  TCP localhost:4380 (LISTEN) alias to list all processes having open ports

--- aliases.sh
+++ aliases.sh
@@ -7,7 +7,4 @@ alias updatedb='sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.l
 alias nosleep='pmset noidle'
 alias chrome='open -a /Applications/Google\ Chrome.app/ $1'
 alias sizes='sudo du -xhd 1'
-alias prod='NODE_CONFIG='{"ldapclient":{"url":"ldap://127.0.0.1:3891","runLocal":"true","dump":"/Users/garbrand/Projects/Dialogica/server/var/ldap/ldapdumpdev"}}' NODE_ENV=production npm start'
-alias sshops="/Users/garbrand/Projects/cxp-ops/ops/bin/ssh_into_opscentre.sh"
-alias talkway="ssh -i ~/.ssh/AWS-key-pair.pem ec2-user@52.57.162.170"
-alias youtrack="ssh -i ~/.ssh/AWS-key-pair.pem ec2-user@52.57.239.26"
+alias ports='sudo lsof -PiTCP -sTCP:LISTEN'
